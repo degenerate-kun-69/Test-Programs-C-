29698dd8292da37c0abe9f1f428746f9ebcc4ef2
Srrijan Arora <srijanarora@hotmail.com>
2024-02-06 00:03:21 +0530

    objectOrientation testing added

---
+++ Test Programs/objectOrientationTest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class Complex {
+    public: double re, im;
+    double norm(){
+        return sqrt(re*re +im*im);
+    }
+    void print(){
+        cout<<"|"<<re<<"+j"<<im<<"| = ";
+        cout<<norm();
+    }
+};
+int main() {
+    Complex c = {4.2,5.3};
+    c.print();
+}
